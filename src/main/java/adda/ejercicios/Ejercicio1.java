e39aeb8d11743f4fa04a7c53bd9c779389fa94b9
Jose <jomaamga@outlook.es>
2023-10-19 13:34:23 +0200

    ejercicio 1

--- src/main/java/adda/ejercicios/Ejercicio1.java
+++ src/main/java/adda/ejercicios/Ejercicio1.java
@@ -12,6 +12,7 @@ public class Ejercicio1 {
 			return EnteroCadena.of(elem.a() + 3,
 					elem.a() % 2 == 0 ? elem.a() + "*" : elem.a() + "!");
 		};
+				
 		return Stream.iterate(EnteroCadena.of(varA, "A"), elem -> elem.a() < varB, nx)
 				.filter(elem -> elem.a() % 10 != 0)
 				.map(elem -> elem.s())
@@ -19,22 +20,17 @@ public class Ejercicio1 {
 	}
 	
 	public static String ejercicio1Iter(Integer varA, Integer varB) {
+		EnteroCadena seed = EnteroCadena.of(varA, "A");
 		StringBuilder res = new StringBuilder();
-		EnteroCadena ec = EnteroCadena.of(varA, "A");
-		int a = ec.a();
-		res.append(ec.s());
-		while (a < varB) {
-			if(a % 10 != 0) {
-				if(a % 2 == 0) {
-					res.append("-");
-					res.append(a + "*");
-				} else {
-					res.append("-");
-					res.append(a + "!");
-				}
+		int a = varA;
+		res.append(seed.s());
+		while(a < varB) {
+			EnteroCadena ec = EnteroCadena.of(a+3, a % 2 == 0 ? a + "*" : a + "!");
+			if(ec.a() % 10 != 0 && ec.a() < varB) {
+				res.append("-").append(ec.s());
 			}
 			a+=3;
-	    }
+		}
 	    return res.toString();
 	}
 }
